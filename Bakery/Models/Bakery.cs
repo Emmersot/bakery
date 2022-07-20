241322e5e1f66a30e12e7d9ffa9f036015ac3722
Tyler Emmerson <tyleremmerson0216@gmail.com>
2022-07-20 16:14:13 -0700

    basic structure.

---
+++ Bakery/Models/Bakery.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Bakery.Models
+{
+  public class Pastry
+  {
+    // properties, constructors, methods, etc. go here
+    public static string Method(string behavior)
+    {
+      return behavior + " is behaving";
+    }
+  }
+  
+  public class Bread
+  {
+    // properties, constructors, methods, etc. go here
+    public static string Method(string behavior)
+    {
+      return behavior + " is behaving";
+    }
+  }
+}
